072b3a0a743da7f618a3c8af5c41f665b7c085b8
I.S.C. JOSÉ CARLOS JOAQUÍN VAZQUEZ - LINUX <iscjovc0125@gmail.com>
2021-01-23 15:46:36 -0600

    Portfolio in construction

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+
+const typed = new Typed('.typed',{
+    stringsElement: '#cadena-text',
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: false,
+    cursorChar:'|',
+    contentType: 'html'
+});
+
+
