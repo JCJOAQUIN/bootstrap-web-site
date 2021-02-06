281955e0d8389bc5ab27f705bf7ca11f4b506553
I.S.C. JOSÉ CARLOS JOAQUÍN VAZQUEZ - LINUX <iscjovc0125@gmail.com>
2021-02-06 12:19:17 -0600

    Pagina en español falta la sección "a cerca de"

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-
+//  *****************      Efecto maquina de escribir     *****************
 
 
 const typed = new Typed('.typed',{
-    stringsElement: '#cadena-text',
+    stringsElement: '#cadena-texto',
     typeSpeed: 75,
     startDelay: 300,
     backSpeed: 75,
@@ -15,5 +15,33 @@ const typed = new Typed('.typed',{
     cursorChar:'|',
     contentType: 'html'
 });
+//  *****************      Fin del Efecto maquina de escribir     *****************
+
+
+
+//  *****************      Colapsado de la barra de navagación     *****************
+
+var btn = document.getElementById('btn-show'),
+    show = document.getElementById('show'),
+    contador = 0;
+
+function cambios()
+{
+    if(contador==0)
+        {
+            show.classList.add('show-bar');
+            contador=1;
+        }
+    else
+        {
+            show.classList.remove('show-bar');
+            contador=0;
+        }
+}
+
+btn.addEventListener('click', cambios, true);
+
+//  *****************     Find del Colapsado de la barra de navagación     *****************
+
 
 
