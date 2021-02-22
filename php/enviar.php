4792f4979e8ff4c98bee30d9b76830caac5dc2ff
I.S.C. JOSÉ CARLOS JOAQUÍN VAZQUEZ <iscjovc0125@gmail.com>
2021-02-22 17:44:08 -0600

    Actualización en debian

---
+++ php/enviar.php
@@ -0,0 +1,24 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$asunto = $_POST['asunto'];
+$message = $_POST['message'];
+
+$header = 'From: ' . $email . "\r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . "\r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "Este mensaje fue enviado por: " . $nombre . "\r\n";
+$message .= "Su e-mail es: " . $email . "\r\n";
+$message .= "Asunto: " . $asunto . "\r\n";
+$message .= "Mensaje: " . $_POST['message'] . "\r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'josecarlos19882546@gmail.com';
+$titulo = 'ejemplo de mensaje';
+
+mail($para, $titulo, utf8_decode($message), $header);
+
+header("Location:index.html");
+?>
